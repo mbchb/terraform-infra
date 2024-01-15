78c177d0d4f4a5797ea7774bd18205ffab0b496d
mbchb <mabrouka.chebbi.pro@gmail.com>
2024-01-15 19:03:18 +0100

    update file

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "*******"
-  secret_key = "*******"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "myec2" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
+    tags = {
+      Name = "my-ec2"
+    }
 }
